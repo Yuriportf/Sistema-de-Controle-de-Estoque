1471301662391bad857689d15284d3c40b6d70cc
Yuri Santos de Oliveira <ysoli7777@gmail.com>
2025-02-13 09:14:57 -0300

    Delete iniciar_flask.bat

--- iniciar_flask.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\yuris\OneDrive\Desktop\Projetos de programação\PROJETOSDEPYTHON\PROJEOSGERAIS\SistemadeControledeEstoque\ProjetoEstoque"
-call .\venv\Scripts\activate.bat
-python app.py
-start http://127.0.0.1:5000
-pause
